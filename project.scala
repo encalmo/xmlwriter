9432f5f8791236bb8815a1daf3719df3698bffa9
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-01-27 20:28:30 +0000

    remove experimental flag

--- project.scala
+++ project.scala
@@ -2,9 +2,6 @@
 //> using exclude scripts
 //> using dep org.encalmo::macro-utils:0.9.2
 //> using test.dep org.scalameta::munit:1.2.1
-//> using test.toolkit 0.8.0
-//> using option -Xmax-inlines 128
-//> using option -experimental
 //> using publish.organization "org.encalmo"
 //> using publish.name "xmlwriter"
 //> using publish.url "https://github.com/encalmo/xmlwriter"
